6dcfd84d7bb5e0eedaf315acefd79e719464a9ff
Bart van Lier <bartvlier@gmail.com>
2020-04-09 13:31:50 +0200

    Auto stash before merge of "feature/reader-view" and "origin/develop"

---
+++ client/src/components/organisms/LetterSize.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="letter-size">
+    <div class="wrapper">
+      <img src="@/assets/svg/letter-size.svg">
+    </div>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'LetterSize',
+};
+</script>
+
+<style scoped lang="scss">
+  .wrapper {
+    max-width: 80%;
+    margin: 0 auto;
+    img {
+      padding: 3rem 0;
+    }
+  }
+</style>

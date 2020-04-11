f9a3a9a697fa0b7f040b14fe4e986472b7d7eb85
Mick Jasker <mick@swurl.nl>
2020-04-11 11:36:26 +0200

    Add PWA config

---
+++ client/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'Leesgeest',
+    themeColor: '#F54F74',
+    msTileColor: '#242582',
+    manifestOptions: {
+      background_color: '#131444'
+    }
+  }
+}

06dab01bdc0bf43833a25bac705e287c1ef14056
Bart van Lier <bartvlier@gmail.com>
2020-04-14 14:46:25 +0200

    Added form + basic styling

--- client/src/views/Forward.vue
+++ client/src/views/Forward.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="forward-view">
-    <h1>Het werkt!</h1>
+    <ForwardForm />
   </div>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-property-decorator';
+import { Vue, Component } from 'vue-property-decorator';
+import ForwardForm from '@/components/organisms/ForwardForm.vue';
+
+@Component({
+  components: {
+    ForwardForm,
+  },
+})
 
 export default class Forward extends Vue {}
 
@@ -13,5 +20,13 @@ export default class Forward extends Vue {}
 
 
 <style scoped lang="scss">
-
+  .forward-view {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    background: #242582;
+    height: 100vh;
+    width: 100%;
+  }
 </style>
